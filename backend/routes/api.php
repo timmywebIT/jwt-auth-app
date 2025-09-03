a4b89d60613e940419b3e48606424246d751243b
timmy-ctrl <timmygithub@gmail.com>
2025-09-03 12:26:05 +0800

    f

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,5 +4,14 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+
+Route::middleware(['auth:api', 'role:admin'])->get('/admin/dashboard', function () {
+    return response()->json(['message' => 'Welcome, Admin']);
+});
+
+Route::middleware(['auth:api', 'role:user'])->get('/user/dashboard', function () {
+    return response()->json(['message' => 'Welcome, User']);
+});
